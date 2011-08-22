6f43ccb5bfb8f5f85ad1733e09d44b3eccde1efd
Jose Luis Caro <jlcaro@gmail.com>
2011-08-22 14:27:23 -0500

    Revert "Initial Commit"
    This reverts commit 7db3d970bc6f29fad103ba93af7d0f5d7d95105d.

--- demo/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Note(models.Model):
-    title = models.CharField(max_length=50)
-    text = models.TextField(null=True, blank=True)
-
-class NoteAttachment(models.Model):
-    file = models.FileField(upload_to='attachments')
-
--- fileupload/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class FileUploadData(object):
-    def __init__(self, name, url, delete_url=None, delete_type='DELETE'):
-        self.name = name
-        self.url = url
-        self.delete_url = delete_url
-        self.delete_type = delete_type
